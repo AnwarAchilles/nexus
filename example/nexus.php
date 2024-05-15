30a568d44c9402fbf5d713feda4c80c491ed6591
AnwarAchilles <hudorianwar07@gmail.com>
2024-05-15 21:30:01 +0700

    update

--- example/nexus.php
+++ example/nexus.php
@@ -14,33 +14,20 @@ Nexus\Engine::$BASEDIR = __DIR__;
 Nexus\Engine::$PROTECT = "123";
 
 
-/* 
- * EXAMPLE 1 file create 
- * */
-Nexus\Source::php("/src/server");
-Nexus\Source::css("/src/style");
-Nexus\Source::html("/src/template");
-Nexus\Source::js("/src/script");
-// Nexus\Source::asset("/src/asset.png");
+Nexus\Environment::set( function() {
+  /* 
+   * EXAMPLE 1 file create 
+   * basic HTML, CSS, JS, PHP, ASSET
+   * */
+  Nexus\Source::php("/src/server");
+  Nexus\Source::css("/src/style");
+  Nexus\Source::html("/src/template");
+  Nexus\Source::js("/src/script");
+  Nexus\Source::asset("/src/asset.png");
+  
+  Nexus\Engine::build('/index', 'class.static', true);
+});
 
-Nexus\Engine::build('/index', 'class.static', true);
+
+Nexus\Environment::run('/index.php');
 Nexus\Engine::watch('/src/');
-
-
-
-/* 
- * EXAMPLE multiple file at 1 time 
- * */
-// Nexus\Environment::set( function() {
-//   Nexus\Source::php("/src/dev1/server");
-//   Nexus\Engine::build('/index', 'plate', true);
-// });
-
-
-// Nexus\Environment::set( function() {
-//   Nexus\Source::php("/src/dev2/server");
-//   Nexus\Engine::build('/index2', 'plate', true);
-// });
-
-// Nexus\Environment::run('/index2');
-// Nexus\Engine::watch('/src/');
